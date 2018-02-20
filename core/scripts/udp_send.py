6a99fa515f916a3173c396e853c87dfed071bdc8
大野木俊樹 <toshiki@LifeIsGood.local>
2018-02-20 12:40:13 +0900

    Add: 文字列をバイト送信できるようにした

--- core/scripts/udp_send.py
+++ core/scripts/udp_send.py
@@ -1,4 +1,6 @@
 import socket
 
+text = "I am Toshiki Ohnogi, studying at Tokyo City University."
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b"hogehoge", ("133.78.120.61", 8000))
+s.sendto(text.encode(), ("133.78.120.61", 8000))
