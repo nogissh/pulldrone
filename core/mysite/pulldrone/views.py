8a808e727ebf95cfb9404d257576bac3398b3151
大野木俊樹 <toshiki@LifeIsGood.local>
2018-02-22 10:13:31 +0900

    Add: ドローンコントローラへのUDP送信に対応

--- core/mysite/pulldrone/views.py
+++ core/mysite/pulldrone/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import datastructures
 
-import ipaddress, socket, time
+import ipaddress, json, socket, time
 import numpy as np
 
 from pulldrone.pypro import warai_decider #笑顔判定クラス
@@ -16,12 +16,27 @@ def index(request):
     content["timelimit"] = request.POST["timelimit"]
     content["player_1_ip"] = request.POST["player_1_ip"]
     content["player_2_ip"] = request.POST["player_2_ip"]
+    content["dronecontroler_ip"] = request.POST["dronecontroler_ip"]
+    content["dronecontroler_port"] = request.POST["dronecontroler_port"]
   except datastructures.MultiValueDictKeyError:
     pass
   return render(request, "pulldrone/index.html", content)
 
 
 def toplay(request):
+
+  # moverangeの送り先
+  w_sendto = {}
+  try:
+    ipaddress.ip_address(request.POST["dronecontroler_ip"])
+    w_sendto["ipaddress"] = request.POST["dronecontroler_ip"]
+  except ValueError:
+    return HttpResponse("IPアドレスが不正です: dronecontroler")
+  try:
+    w_sendto["port"] = int(request.POST["dronecontroler_port"])
+  except ValueError:
+    return HttpResponse("ポート番号には半角数字をいれてくださいs")
+
 
   # 制限時間
   try:
@@ -47,6 +62,9 @@ def toplay(request):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.bind(("", 8080))
   s.setblocking(0)
+
+  # ソケットを用意（送信のみ、UDP）
+  sSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
   # UDP受信のための変数を初期化
   data = ""
@@ -87,7 +105,14 @@ def toplay(request):
           player_2.waiting = False
 
           """ ドローン移動命令 """
-          #send(moverange)
+          content = {
+
+            "sentfrom": "133.78.120.61",
+            "moverange": moverange,
+          }
+          content = json.dumps(content)
+          sSend.sendto(content.encode(),
+            (w_sendto["ipaddress"], w_sendto["port"]))
           """ /ドローン移動命令 """
 
         except ValueError:
@@ -103,6 +128,8 @@ def toplay(request):
     "timelimit": str(timelimit),
     "player_1_ip": player_1.playerAddress,
     "player_2_ip": player_2.playerAddress,
+    "dronecontroler_ip": w_sendto["ipaddress"],
+    "dronecontroler_port": w_sendto["port"],
   }
   del player_1, player_2 #クラスの削除
   return render(request, "pulldrone/finished.html", content)
